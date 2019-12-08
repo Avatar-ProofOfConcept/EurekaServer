3e83d5a6128039b82347280629e57efa2d30b3af
Amalguittoum <fa_guittoum@esi.dz>
2019-12-08 15:54:06 +0100

    Deploiement of Eureka server

---
+++ src/main/java/fr/laas/sevicediscovery/SeviceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package fr.laas.sevicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SeviceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeviceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/laas/sevicediscovery/SeviceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.laas.sevicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeviceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
